0c02ffe6ef8912fa3ff59efcecffb0a1f4cfe98c
Galahad L <galahadl06@outlook.com>
2024-10-09 22:22:06 +0800

    Migrated from Duo-Calc Shrink code length using function to replace inputing parts. Add new operator:square root.

--- Duo Calc POSIX/main.c
+++ Duo Calc POSIX/main.c
@@ -4,11 +4,145 @@
 //
 //  Created by Galahad L on 2024/10/9.
 //
-
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <math.h>
+#include <unistd.h>
+#include <string.h>
+float max(float a,float b){
+    float max;
+    if (a>b) {
+        max = a;
+    } else {
+        max = b;
+    }
+    return max;
+}
+float min(float a,float b){
+    float min;
+    if (a<b) {
+        min = a;
+    } else {
+        min = b;
+    }
+    return min;
+}
+int intbasicinput1(int a){
+    printf("Input a :");
+    scanf("%d",&a);
+    return a;
+}
+float floatbasicinput1(float a){
+    printf("Input a :");
+    scanf("%f",&a);
+    return a;
+}
+int intbasicinput2(int b){
+    printf("Input b :");
+    scanf("%d",&b);
+    return b;
+}
+float floatbasicinput2(float b){
+    printf("Input b :");
+    scanf("%f",&b);
+    return b;
+}
+float max(float a,float b);
+float min(float a,float b);
+int intbasicinput1(int a);
+int intbasicinput2(int b);
+float floatbasicinput1(float a);
+float floatbasicinput2(float b);
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    float a = 1;
+    float b = 1;
+    char c ;
+    int count = 0;
+    system("clear");
+    printf("Welcome to use Duo-Calc!\n");
+    sleep(1);
+    system("clear");
+    while(1){
+        printf("Choose from '+' '-' '*' '/' '%%' '!' '^' 'sqrt'\nPress '$' to exit\nInput an operator:");
+        scanf("%s",&c);
+        if(c=='M'){
+            float a = floatbasicinput1(a = 0),b = floatbasicinput2(b = 0);
+            float i =max(a,b);
+            printf("Max = %f\n",i);
+        }
+        else if(c=='m'){
+            float a = floatbasicinput1(a = 0),b = floatbasicinput2(b = 0);
+            float i = min(a,b);
+            printf("Min = %f\n",i);
+        }
+        else if(c=='+'){
+            float a = floatbasicinput1(a = 0),b = floatbasicinput2(b = 0);
+            printf("a+b=%f\n",a+b);
+        }
+        else if(c=='-'){
+            float a = floatbasicinput1(a = 0),b = floatbasicinput2(b = 0);
+            printf("a-b=%f\n",a-b);
+        }
+        else if(c=='*'){
+            float a = floatbasicinput1(a = 0),b = floatbasicinput2(b = 0);
+            printf("a*b=%f\n",a*b);
+        }
+        else if(c=='/'){
+            float a = floatbasicinput1(a = 0),b = floatbasicinput2(b = 0);
+            if(b==0){
+                printf("Invalid usage of operator '/'\n");
+            }else{
+                printf("a/b=%f\n",a/b);
+            }
+        }
+        else if(c=='%'){
+            int c = intbasicinput1(a = 0),e = intbasicinput2(b = 0);
+            double f = c%e;
+            printf("a%%b=%f\n",f);
+        }
+        else if(c=='!'){
+            int x = intbasicinput1(x = 0);
+            double result = 1;
+            if (a<0) {
+                printf("Invalid usage of operator '!'");
+            }
+            else{
+                for (int i = 1; i <= x; i++) {
+                    result =result* i;
+                }
+                printf("a!=%e\n",result);
+            }
+        }
+        else if (c=='^'){
+            int x = intbasicinput1(a = 0),y = intbasicinput2(b = 0);
+            double g = pow(x,y);
+            printf("a^b=%e\n",g);
+        }
+        else if (strcmp(&c,"sqrt")==0){
+            float a = floatbasicinput1(a = 0);
+            if(a<0){
+                printf("Invalid usage of square root.");
+            }
+            else{
+                printf("The square root of a is %f\n",sqrt(a));
+            }
+        }
+        else if (c=='$'){
+            break;
+        }
+        else{
+            printf("Invalid Operator\n");
+        }
+        printf("\n");
+        getchar();
+        count++;
+        sleep(1);
+        system("clear");
+        sleep(1);
+    }
+    printf("\n********** Program Ended **********\n");
+    printf("Total Circulation:%d\n",count);
     return 0;
 }
+
+
